fd381d43dba1bf8c72580482d6887994de5baed4
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-07-30 01:16:21 +0800

    更新

--- MyAssert.hpp
+++ MyAssert.hpp
@@ -3,20 +3,21 @@
 #include <stdio.h>
 #include <varargs.h>
 
-void __MyAssert_Function__(const char *pFileName, size_t szLine, const char *pFunctionName, bool bValid, _Printf_format_string_ const char *pInfo, ...)
+//inline防止重定义，_Printf_format_string_用于触发编译器参数类型匹配检查
+inline void __MyAssert_Function__(const char *pFileName, size_t szLine, const char *pFunctionName, bool bValid, _Printf_format_string_ const char *pInfo, ...)
 {
 	if (bValid)
 	{
-		return;
+		return;//校验成功返回
 	}
 
-	printf("Assertion Failure!\n\tin file: %s\n\tin line: %zu\n\tin func: %s\n\tin info: ", pFileName, szLine, pFunctionName);
-
+	printf("Assertion Failure!\n    in file: %s\n    in line: %zu\n    in func: %s\n    in info: ", pFileName, szLine, pFunctionName);
+	
 	va_list vl;
 	va_start(vl, pInfo);
 	vprintf(pInfo, vl);
 	va_end(vl);
-
+	//帮忙换行
 	putchar('\n');
 
 	exit(-114514);
@@ -25,6 +26,6 @@ void __MyAssert_Function__(const char *pFileName, size_t szLine, const char *pFu
 //代理宏，延迟展开
 #define __MY_ASSERT_LINE__ __LINE__
 #define __MY_ASSERT_FILE__ __FILE__
-#define __MY_ASSERT_FUNC__ __FUNCTION__
+#define __MY_ASSERT_FUNC__ __FUNCSIG__//使用完整函数名
 
 #define MyAssert(v, i, ...) __MyAssert_Function__(__MY_ASSERT_FILE__, __MY_ASSERT_LINE__, __MY_ASSERT_FUNC__, (v), (i), __VA_ARGS__)
