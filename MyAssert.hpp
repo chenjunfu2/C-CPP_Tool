a64946c0b1fbdb4393282b73b4239e3aa5883cae
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-10-15 21:56:44 +0800

    修改宏定义以支持不同编译器

--- MyAssert.hpp
+++ MyAssert.hpp
@@ -3,7 +3,49 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-inline void __MyAssert_Function__(const char *pFileName, size_t szLine, const char *pFunctionName, bool bValid, const char *pInfo = NULL, ...)
+//必须没有提前定义过任何此类宏
+#if !defined(__COMPILER_MSVC__) &&\
+	!defined(__COMPILER_GCC__) &&\
+	!defined(__COMPILER_CLANG__)
+
+	//先预定义所有可能的编译器宏
+	#define __COMPILER_MSVC__ 0
+	#define __COMPILER_GCC__ 0
+	#define __COMPILER_CLANG__ 0
+	
+	//后实际判断是哪个编译器，是就替换它自己的宏为1
+	#if defined(_MSC_VER)
+		#undef  __COMPILER_MSVC__
+		#define __COMPILER_MSVC__ 1
+		#define __COMPILER_NAME__ "MSVC"
+	#elif defined(__GNUC__)
+		#undef  __COMPILER_GCC__
+		#define __COMPILER_GCC__ 1
+		#define __COMPILER_NAME__ "GCC"
+	#elif defined(__clang__)
+		#undef  __COMPILER_CLANG__
+		#define __COMPILER_CLANG__ 1
+		#define __COMPILER_NAME__ "Clang"
+	#else
+		#define __COMPILER_NAME__ "Unknown"
+	#endif
+#endif
+
+
+#if __COMPILER_MSVC__
+#define PRINTF_FORMAT_ARGS _Printf_format_string_
+#define PRINTF_FORMAT_ATTR
+#elif __COMPILER_GCC__ || __COMPILER_CLANG__
+#define PRINTF_FORMAT_ARGS
+#define PRINTF_FORMAT_ATTR __attribute__((__format__ (__printf__, 5, 6)))
+#else
+#define PRINTF_FORMAT_ARGS
+#define PRINTF_FORMAT_ATTR
+#endif
+
+
+PRINTF_FORMAT_ATTR
+inline void __MyAssert_Function__(const char *pFileName, size_t szLine, const char *pFunctionName, bool bValid, PRINTF_FORMAT_ARGS const char *pInfo = NULL, ...)
 {
 	if (bValid)
 	{
@@ -35,6 +77,14 @@ inline void __MyAssert_Function__(const char *pFileName, size_t szLine, const ch
 #define __MY_ASSERT_LINE__ __LINE__
 #define __MY_ASSERT_FUNC__ __FUNCTION__
 
-//cpp20 __VA_OPT__(,)，仅在__VA_ARGS__不为空时添加','以防止编译错误
-//msvc需启用/Zc:preprocessor以使得预处理器识别此宏关键字
+//cpp20的__VA_OPT__(,)，仅在__VA_ARGS__不为空时添加','以防止编译错误
+//msvc需启用"/Zc:preprocessor"以使得预处理器识别此宏关键字（哎呀msvc你怎么这么坏呀）
 #define MyAssert(v, ...) __MyAssert_Function__(__MY_ASSERT_FILE__, __MY_ASSERT_LINE__, __MY_ASSERT_FUNC__, (v) __VA_OPT__(,) __VA_ARGS__)
+
+#undef PRINTF_FORMAT_ATTR
+#undef PRINTF_FORMAT_ARGS
+
+#undef __COMPILER_NAME__
+#undef __COMPILER_CLANG__
+#undef __COMPILER_GCC__
+#undef __COMPILER_MSVC__
